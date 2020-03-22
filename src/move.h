f31618786a6f718b39a2565185bec245af322012
ZGHARI Hicham <Hicham.Zghari@bordeaux-inp.fr>
2020-03-22 21:44:18 +0100

    new move.h

--- src/move.h
+++ src/move.h
@@ -4,14 +4,13 @@
 #include <stdlib.h>
 
 /* Players */
-enum color_t { BLACK=0, WHITE=1 };
+enum color_t { BLACK=0, WHITE=1, NO_COLOR=2 };
 
 /* Positions */
-struct move_t { size_t v; }; // An index between 0 and the number of vertices of - 1
-
-struct col_move_t {
-  struct move_t m;
-  enum color_t c;
+struct move_t {
+  size_t m;       // An index between 0 and the number of vertices of - 1
+  enum color_t c; // The color of the player
 };
 
 #endif // _HEX_MOVE_H_
+
