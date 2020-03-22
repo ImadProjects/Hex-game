cab1b1e743fcb9d23471519c40c2774f98c1160a
ZGHARI Hicham <Hicham.Zghari@bordeaux-inp.fr>
2020-03-22 16:42:17 +0100

    replace = by ==

--- src/graph.c
+++ src/graph.c
@@ -47,7 +47,7 @@ size_t width__graph_t(struct graph_t *graph){
   size_t n = size__graph_t(graph);
   float m = sqrt(n);
   
-  if (m = (float)floor(m))
+  if (m == (float)floor(m))
 
     return m - 1;
 
