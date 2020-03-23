0849af1c4ab583bf242721c94ad69081573e2df9
NGUYEN Olivier <Olivier.Nguyen@bordeaux-inp.fr>
2020-03-23 17:36:36 +0100

    updated server by adding is_move_possible and punishing wrong moves

--- src/server.c
+++ src/server.c
@@ -32,6 +32,15 @@ void parse_opts(int argc, char* argv[]) {
   }
 }
 
+int is_move_possible(struct graph_t* g, int color, struct move_t move){
+  int n = (int) move.m;
+  if ((gsl_spmatrix_get(g->o, 0, n)==0) && (gsl_spmatrix_get(g->o, 1, n) == 0)){
+    return 0;
+  }
+  return 1;
+}
+
+
 struct player{
   
   char const *name;
@@ -67,84 +76,104 @@ int main(int argc,  char* argv[]){
   printf("\nLength : %d\n", Length);
   printf("shape : %d\n", Shape);
 //-------------------------------------------
-        struct graph_t *graph = new__graph_t(Length, Shape); 
-	void * player1 = dlopen("install/player1.so",RTLD_NOW);
-	void * player2 = dlopen("install/player2.so",RTLD_NOW);
+  struct graph_t *graph = new__graph_t(Length, Shape); 
+  void * player1 = dlopen("install/player1.so",RTLD_NOW);
+  void * player2 = dlopen("install/player2.so",RTLD_NOW);
+  
+  struct player * p1 = dlsym(player1,"player1");
+  
+  p1->propose_opening = dlsym(player1,"propose_opening");
+  p1->accept_opening = dlsym(player1,"accept_opening");
+  p1->initialize_color = dlsym(player1,"initialize_color");
+  p1->initialize_graph = dlsym(player1,"initialize_graph");
+  p1->play = dlsym(player1,"play");
+  p1->finalize = dlsym(player1,"finalize");
+  p1->initialize_graph(graph);
 
-	struct player * p1 = dlsym(player1,"player1");
+  struct player * p2 = dlsym(player2,"player2");
 
-	p1->propose_opening = dlsym(player1,"propose_opening");
-	p1->accept_opening = dlsym(player1,"accept_opening");
-	p1->initialize_color = dlsym(player1,"initialize_color");
-	p1->initialize_graph = dlsym(player1,"initialize_graph");
-	p1->play = dlsym(player1,"play");
-	p1->finalize = dlsym(player1,"finalize");
-	p1->initialize_graph(graph);
-
-	struct player * p2 = dlsym(player2,"player2");
-
-	p2->propose_opening = dlsym(player2,"propose_opening");
-	p2->accept_opening = dlsym(player2,"accept_opening");
-	p2->initialize_color = dlsym(player2,"initialize_color");
-	p2->initialize_graph = dlsym(player2,"initialize_graph");
-	p2->play = dlsym(player2,"play");
-	p2->finalize = dlsym(player2,"finalize");
-	p2->initialize_graph(graph);
+  p2->propose_opening = dlsym(player2,"propose_opening");
+  p2->accept_opening = dlsym(player2,"accept_opening");
+  p2->initialize_color = dlsym(player2,"initialize_color");
+  p2->initialize_graph = dlsym(player2,"initialize_graph");
+  p2->play = dlsym(player2,"play");
+  p2->finalize = dlsym(player2,"finalize");
+  p2->initialize_graph(graph);
 	
-	struct move_t move = p1->propose_opening();
-	//struct col_move_t * last_move;
-	//last_move->m = move;
-	//last_move->c = 0;
+  struct move_t move = p1->propose_opening();
+  //struct col_move_t * last_move;
+  //last_move->m = move;
+  //last_move->c = 0;
 
-	p1->initialize_color(1 - p2->accept_opening(move));
-	p2->initialize_color(p2->accept_opening(move));
+  p1->initialize_color(1 - p2->accept_opening(move));
+  p2->initialize_color(p2->accept_opening(move));
 	
-	int select_player = 0;
-	struct move_t t ={1};
-	while (1){
-
-	  srand(time(NULL));
+  int select_player = 0;
+  int end_by_impossible_move = 1;
+  struct move_t t ={1};
+  while (1){
 	  
-	  if (select_player==0)
-		{
-			select_player++;	
-			move = p1->play(move);
-			coloriate__graph_t(graph, 0, t);
-			print_graph(graph, 'c');
-			t.m++;
-			//printf("%d\n",select_player);
-			//printf("%d\n",t.v++); debug
-		}
-		else
-		{
-			select_player++;	
-			move = p2->play(move);
-			print_graph(graph, 'c');
-			select_player %=2;
-		}
-			//printf("%d\n",select_player); debug
-
-	 /* struct player * current_player = compute_next_player(player1,player2,last_move);
-	  move = current_player->play(move);
-	  last_move->m = move;
-	  last_move->c = current_player->color;
-	  coloriate__graph_t(graph, current_player->color, t);
-	  t.v++;*/
-	  print_graph(graph, 'c');
-	  if (is_winning(graph,0,move,'c') || is_winning(graph,1,move,'c'))
-			break;
-	}
-	
-	if (is_winning(graph,0,move,'c')==0 )
+    srand(time(NULL));
 	  
-	  printf("The winner is the player 1\n");
-	
+    if (select_player==0)
+      {
+	select_player++;	
+	move = p1->play(move);
+	if(is_move_possible(graph,p1->color,move))
+	  {
+	    coloriate__graph_t(graph, 0, t);
+	    print_graph(graph, 'c');
+	    t.m++;
+	  }
 	else
-	  
-	  printf("The winner is the player 2\n");
+	  {
+	    printf("The winner is player 2, player 1 chose a wrong move\n");
+	    end_by_impossible_move = 0;
+	    break;
+	  }
+	//printf("%d\n",select_player);
+	//printf("%d\n",t.v++); debug
+      }
+    else
+      {
+	select_player++;	
+	move = p2->play(move);
+	if(is_move_possible(graph,p2->color,move))
+	  {
+	    print_graph(graph, 'c');
+	    select_player %=2;
+	  }
+	else
+	  {
+	    printf("The winner is player 1, player 2 chose a wrong move\n");
+	    end_by_impossible_move = 0;
+	    break;
+	  }
+      }
+    //printf("%d\n",select_player); debug
 
-	free__graph_t(graph);
-	dlclose(player1);
-	dlclose(player2);
-	return 0;
+    /* struct player * current_player = compute_next_player(player1,player2,last_move);
+       move = current_player->play(move);
+       last_move->m = move;
+       last_move->c = current_player->color;
+       coloriate__graph_t(graph, current_player->color, t);
+       t.v++;*/
+    print_graph(graph, 'c');
+    if (is_winning(graph,0,move,'c') || is_winning(graph,1,move,'c'))
+      break;
+  }
+  if(end_by_impossible_move)
+    {
+      if (is_winning(graph,0,move,'c')==0 )
+	      
+	printf("The winner is the player 1\n");
+	    
+      else
+	      
+	printf("The winner is the player 2\n");
+    }
+  free__graph_t(graph);
+  dlclose(player1);
+  dlclose(player2);
+  return 0;
 }
