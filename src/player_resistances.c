6c2bc3ca2a7bd4444ae473ad33e78c60e7c45039
Augustin Gauchet <Augustin.Gauchet@bordeaux-inp.fr>
2020-05-09 15:13:57 +0200

    player_resistance now works with a square board

--- src/player_resistances.c
+++ src/player_resistances.c
@@ -1,7 +1,9 @@
 #include "player.h"
 #include "resistance.h"
 
-int is_move_possible(struct graph_t* g, int color, struct move_t move);
+int is_possible(struct graph_t* g, int color, struct move_t move){
+  return (gsl_spmatrix_get(g->o, 0, (int) move.m) == 0 && gsl_spmatrix_get(g->o, 1, (int) move.m) == 0);
+}
 
 struct player{
 
@@ -12,23 +14,18 @@ struct player{
   
 };
 
-struct player jeremy = {.name ="Xx_JérémY_xX"};
-
+struct player jeremy = {.name ="Jérémy"};
 char const *get_player_name(){
-
   return jeremy.name;
 }
 
 void initialize_color(enum color_t id){
-
   jeremy.color = id;
   coloriate__graph_t(jeremy.graph, 0, jeremy.last_move);
 }
 
 
 void initialize_graph(struct graph_t* graph){
-
-
   jeremy.graph = graph;   
 }
 
@@ -51,11 +48,12 @@ struct move_t propose_opening(){//temporaire
 
   struct move_t opening;
   opening.c = jeremy.color;
-  opening.m = size__graph_t(jeremy.graph) / 2;
+  opening.m = 6; // sqrt(jeremy.graph->num_vertices) + 2;
   return opening;
 }
 
 struct move_t play(struct move_t previous_move){
+  coloriate__graph_t(jeremy.graph, jeremy.color == 0, previous_move);
   struct move_t mec;
   mec.c = jeremy.color;
   int best = -1;
@@ -63,7 +61,7 @@ struct move_t play(struct move_t previous_move){
   double best_ratio = (double) sign * 2000000000.;
   for (int i = 0; i < (jeremy.graph)->num_vertices; i++){
     mec.m = i;
-    if (is_move_possible(jeremy.graph, mec.c, mec)){
+    if (is_possible(jeremy.graph, mec.c, mec)){
       double ratio = get_ratio(jeremy.graph, mec);
       if (!mec.c){
 	if ((ratio < best_ratio) && (ratio >= 0)){
@@ -82,10 +80,11 @@ struct move_t play(struct move_t previous_move){
   }
   if (best < 0){
     printf("=== error, jeremy can't find a proper place to play ===\n");
-    mec.m = best;
+    mec.m = sqrt(jeremy.graph->num_vertices) + 1;
     return mec;
   }
   mec.m = best;
+  coloriate__graph_t(jeremy.graph, jeremy.color, mec);
   return mec;
 }
 
