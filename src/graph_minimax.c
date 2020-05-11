00f55449f3db6399693cb90c81d30ce615ac3f42
ZGHARI Hicham <Hicham.Zghari@bordeaux-inp.fr>
2020-05-12 00:50:19 +0200

    update minimax

--- src/graph_minimax.c
+++ src/graph_minimax.c
@@ -5,72 +5,51 @@
 struct dynamic_array *neighbours(struct graph_t *graph, int position)
 {
   struct dynamic_array *neighbours = empty__dynamic_array();
+  
   for (int k = 0; k < graph->num_vertices; k++)
-  {
     if (gsl_spmatrix_get(graph->t, position, graph->num_vertices - k - 1) == 1)
       add__to_dynamic_array(neighbours, graph->num_vertices - k - 1);
-  }
+  
   return neighbours;
 }
 
 int max_a_b(int a, int b)
 {
-  if (a >= b)
-    return a;
-  else
-    return b;
+  
+    return (a >= b)? a: b;
 }
 
 int min_a_b(int a, int b)
 {
-  if (a <= b)
-    return a;
-  else
-    return b;
+
+    return (a <= b)? a: b;
 }
 
 struct dynamic_array *path_intesection(struct dynamic_array *p1, struct dynamic_array *p2)
 {
+  
   struct dynamic_array *inter = empty__dynamic_array();
-  size_t i = 0;
-  if (p1->size < p2->size)
-  {
-    for (i = 0; i < p1->size; i++)
-    {
+  
+    for (size_t i = 0; i < p1->size; i++)
       for (size_t j = 0; j < p2->size; j++)
-      {
         if (p1->array[i] == p2->array[j])
-        {
           add__to_dynamic_array(inter, p1->array[i]);
-          continue;
-        }
-      }
-    }
-  }
-  else
-  {
-    for (i = 0; i < p2->size; i++)
-    {
-      for (size_t j = 0; j < p1->size; j++)
-      {
-        if (p2->array[i] == p1->array[j])
-        {
-          add__to_dynamic_array(inter, p2->array[i]);
-          continue;
-        }
-      }
-    }
-  }
+
   return inter;
 }
 
 struct dynamic_array *path_union(struct dynamic_array *p1, struct dynamic_array *p2)
 {
+  
+ struct dynamic_array *unio = empty__dynamic_array();
+
+ for (size_t i = 0; i < p1->size; i++)
+    add__to_dynamic_array(unio, p1->array[i]);
+ 
   for (size_t i = 0; i < p2->size; i++)
-  {
-    add__to_dynamic_array(p1, p2->array[i]);
-  }
-  return p1;
+    add__to_dynamic_array(unio, p2->array[i]);
+  
+  return unio;
 }
 
 int minimax(struct graph_t *G, struct graph_t *graph_player, int maxminplayer, int depth, int alpha, int beta, enum color_t id, int M)
@@ -78,11 +57,9 @@ int minimax(struct graph_t *G, struct graph_t *graph_player, int maxminplayer, i
   struct dynamic_array *player_path = djikstra(graph_player, M, (1 - id) * (M + 1), (1 - id));
   struct dynamic_array *enemy_path = djikstra(graph_player, M, ((1 - id) + 1) % 2 * (M + 1), ((1 - id) + 1) % 2);
   struct dynamic_array *intersection = path_intesection(player_path, enemy_path);
+  
   if (depth == 0 || intersection->size == 0 || game_over(G, id) || game_over(G, 1 - id))
-  {
-    int h = path_quality(G, M, (1 - id));
-    return h;
-  }
+    return path_quality(G, M, (1 - id));
 
   else if (maxminplayer)
   {
@@ -92,9 +69,7 @@ int minimax(struct graph_t *G, struct graph_t *graph_player, int maxminplayer, i
       if (!is_taken(G->o, intersection->array[i]))
       {
         count++;
-        struct move_t move;
-        move.m = intersection->array[i];
-        move.c = id;
+        struct move_t move = {.m = intersection->array[i], .c = id};
         struct graph_t *copy = copy_new_graph(G, move);
         max = max_a_b(max, minimax(copy, graph_player, 0, depth - 1, alpha, beta, 1 - id, M));
         free__graph_t(copy);
@@ -116,9 +91,7 @@ int minimax(struct graph_t *G, struct graph_t *graph_player, int maxminplayer, i
       if (!is_taken(G->o, intersection->array[i]))
       {
         count2++;
-        struct move_t move;
-        move.m = intersection->array[i];
-        move.c = id;
+        struct move_t move = {.m = intersection->array[i], .c = id};
         struct graph_t *copy = copy_new_graph(G, move);
         min = min_a_b(min, minimax(copy, graph_player, 1, depth - 1, alpha, beta, 1 - id, M));
         free__graph_t(copy);
@@ -137,24 +110,18 @@ int minimax(struct graph_t *G, struct graph_t *graph_player, int maxminplayer, i
 struct move_t best_move(struct graph_t *graph_player, enum color_t id, int M, struct dynamic_array *inter)
 {
   int bestval = INFINIT;
-  struct move_t m;
-  m.c = id;
+  struct move_t m = {.c = id};
 
   for (size_t i = 0; i < inter->size; i++)
   {
     if (!is_taken(graph_player->o, inter->array[i]))
     {
-      struct move_t move;
-      move.m = inter->array[i];
-      move.c = id;
+      struct move_t move = {.m = inter->array[i], .c = id};
       struct graph_t *copy = copy_new_graph(graph_player, move);
       int val = minimax(copy, graph_player, 1, 50, -INFINIT, INFINIT, 1 - id, M);
       free__graph_t(copy);
       if (val < bestval)
-      {
-        bestval = val;
-        m.m = inter->array[i];
-      }
+        bestval = val, m.m = inter->array[i];
     }
   }
   return m;
--- src/path.c
+++ src/path.c
@@ -4,48 +4,43 @@
 int find_min_distance(int *distance, int *selected, int num_vertices)
 {
   int min = INFINIT;
-  int vertex = -1;
+  int min_index = -1;
+  
   for (int k = 0; k < num_vertices; k++)
-  {
     if (distance[k] <= min && selected[k] == 0)
-    {
-      min = distance[k];
-      vertex = k;
-    }
-  }
-  return vertex;
+      min = distance[k], min_index = k;
+   
+  return min_index;
 }
 
-struct dynamic_array *djikstra(struct graph_t *G, size_t M, int src, enum color_t id)
+struct dynamic_array *djikstra(struct graph_t *G, size_t M, int src, enum color_t color)
 {
-  int dist[G->num_vertices];
-
-  int parent[G->num_vertices];
-  for (size_t i = 0; i < G->num_vertices; i++)
-  {
-    parent[i] = INFINIT;
-  }
-
-  int sptSet[G->num_vertices];
-  for (size_t i = 0; i < G->num_vertices; i++)
-    dist[i] = INFINIT, sptSet[i] = 0;
-
-  dist[src] = 0;
+  int vertices = G->num_vertices;
+  int distance[vertices];
+  int parent[vertices];
+  int selected[vertices];
+  
+  for (size_t i = 0; i < vertices; i++)
+    parent[i] = INFINIT, distance[i] = INFINIT, selected[i] = 0;
+      
+  distance[src] = 0;
   parent[src] = -1;
 
   for (int count = 0; count < (int)(G->num_vertices - 1); count++)
   {
-
-    int u = find_min_distance(dist, sptSet, G->num_vertices);
-
-    sptSet[u] = 1;
+    int u = find_min_distance(distance, selected, G->num_vertices);
+    selected[u] = 1;
 
     for (size_t v = 0; v < G->num_vertices; v++)
     {
-
-      if (!sptSet[v] && gsl_spmatrix_get(G->t, u, v) && dist[u] != INFINIT && !gsl_spmatrix_get(G->o, (id + 1) % 2, v) && dist[u] + (int)(gsl_spmatrix_get(G->t, u, v) - gsl_spmatrix_get(G->o, id, v) * gsl_spmatrix_get(G->o, id, u)) <= dist[v])
+      int d = (int)(gsl_spmatrix_get(G->t, u, v) - gsl_spmatrix_get(G->o, color, v) * gsl_spmatrix_get(G->o, color, u));
+      if (!selected[v] &&
+	  gsl_spmatrix_get(G->t, u, v) &&
+	  distance[u] != INFINIT &&
+	  !gsl_spmatrix_get(G->o, (color + 1) % 2, v) &&
+	  distance[u] + d  <= distance[v])
       {
-        dist[v] = dist[u] + (int)(gsl_spmatrix_get(G->t, u, v) - gsl_spmatrix_get(G->o, id, v) * gsl_spmatrix_get(G->o, id, u));
+        distance[v] = distance[u] + d;
         parent[v] = u;
       }
     }
@@ -53,16 +48,14 @@ struct dynamic_array *djikstra(struct graph_t *G, size_t M, int src, enum color_
 
   struct dynamic_array *p = empty__dynamic_array();
   int i;
-  if (id == BLACK)
+  if (color == BLACK)
     i = G->num_vertices - 1;
   else
     i = 2 * M + 1;
 
   while (i != -1)
-  {
-    add__to_dynamic_array(p, i);
-    i = parent[i];
-  }
+    add__to_dynamic_array(p, i), i = parent[i];
+ 
 
   return p;
 }
@@ -70,11 +63,11 @@ struct dynamic_array *djikstra(struct graph_t *G, size_t M, int src, enum color_
 int path_score(struct dynamic_array *path, struct graph_t *G)
 {
   size_t score = 0, i = 0;
+  
   for (i = 0; i < path->size; i++)
-  {
     if ((gsl_spmatrix_get(G->o, 0, path->array[i]) == 0) && ((gsl_spmatrix_get(G->o, 1, path->array[i]) == 0)))
       score++;
-  }
+  
   return score;
 }
 
@@ -82,13 +75,16 @@ int path_quality(struct graph_t *G, int M, int color)
 {
   if (game_over(G, color))
     return -INFINIT;
+  
   if (game_over(G, (color + 1) % 2))
     return INFINIT;
+  
   struct dynamic_array *p_path = djikstra(G, M, color * (M + 1), color);
   struct dynamic_array *op_path = djikstra(G, M, (color + 1) % 2 * (M + 1), (color + 1) % 2);
-  int p_score = path_score(p_path, G);
-  int op_score = path_score(op_path, G);
+
+  int d = path_score(p_path, G) - path_score(op_path, G); 
   free__dynamic_array(p_path);
   free__dynamic_array(op_path);
-  return p_score - op_score;
+  
+  return d;
 }
