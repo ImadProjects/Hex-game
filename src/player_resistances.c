177fc0bf786d48ad23788912bd0505f10a4729ef
Augustin Gauchet <Augustin.Gauchet@bordeaux-inp.fr>
2020-05-12 18:21:11 +0200

    reduction de la profondeur de jeu de jeour_resistances

--- src/player_resistances.c
+++ src/player_resistances.c
@@ -24,15 +24,6 @@ int* voisins(struct graph_t* g, int i, char type){
   l[3] = i - n + 1;
   l[4] = i + n;
   l[5] = i + n - 1;
-  int ind = 6;
-  for (int j = 0; j < 3; j++){
-    l[ind] = i - 2*n + j;
-    l[ind + 1] = i + 2*n + j;
-    ind += 2;
-  }
-  l[12] = i + 2;
-  l[13] = i - 2;
-  //  for (int j = 0; j < 14; printf("%d\n", l[j++]));
   return l;
 }
   
@@ -55,7 +46,6 @@ char const *get_player_name()
 void initialize_color(enum color_t id)
 {
   jeremy.color = id;
-  //  printf("case coloriée: %ld\n", jeremy.last_move.m);
   coloriate__graph_t(jeremy.graph, jeremy.last_move.c ,jeremy.last_move);
 }
 
@@ -63,8 +53,6 @@ void initialize_graph(struct graph_t *graph)
 {
   jeremy.graph = graph;
 }
-
-//struct move_t play(struct move_t previous_move){
 
 int accept_opening(const struct move_t opening)
 {
@@ -80,13 +68,11 @@ void finalize()
 }
 
 struct move_t propose_opening()
-{ //temporaire
-
+{
   struct move_t opening;
   opening.c = jeremy.color;
   opening.m = sqrt(jeremy.graph->num_vertices) + 1;
   jeremy.last_move = opening;
-  //  printf("opening\n");
   return opening;
 }
 
@@ -105,9 +91,7 @@ struct move_t play(struct move_t previous_move)
   double sign = 1. - 2. * (mec.c == 1);
   double best_ratio = (double)sign * 2000000000.;
   int* voisin = voisins(jeremy.graph , previous_move.m, type);
-  //    printf("previous: %ld\n", previous_move.m);
-  
-  for (int i = 0; i < 4 + 9 * (type == 'h'); i++) { //cases proches
+  for (int i = 0; i < 4 + 2 * (type == 'h'); i++) { //cases proches
     int valide = 1;
     if (voisin[i] >= jeremy.graph->num_vertices || voisin[i] < 0){
       valide = 0;
@@ -115,7 +99,6 @@ struct move_t play(struct move_t previous_move)
     else{
       mec.m = voisin[i];
     }
-    //    printf("%d -> %d\n", voisin[i], is_possible(jeremy.graph, mec.c, mec));
     if (valide){
       if (is_possible(jeremy.graph, mec.c, mec)){
 	double ratio = get_ratio(jeremy.graph, mec);
@@ -144,30 +127,6 @@ struct move_t play(struct move_t previous_move)
 
     return mec;
   }
-  /*  if (best < 0){    //autres cases
-    for (int i = 0; i < jeremy.graph->num_vertices; i++) { //cases lointaines
-      mec.m = i;
-      if (is_possible(jeremy.graph, mec.c, mec)){
-	double ratio = get_ratio(jeremy.graph, mec);
-	if (!mec.c){
-	  if ((ratio < best_ratio) && (ratio >= 0)){
-	    coloriate__graph_t(jeremy.graph, jeremy.color, mec);
-	    //	    printf("move: %ld\n", mec.m);
-	    return mec;
-	  }
-	}
-	else{
-	  if ((ratio > best_ratio) && (ratio >= 0)){
-	    coloriate__graph_t(jeremy.graph, jeremy.color, mec);
-	    //	    printf("move: %ld\n", mec.m);
-	    return mec;
-	  }
-	}
-      }
-    }
-  }*/
-         
-  //  printf("=== error, jeremy can't find a proper place to play ===\n");
   for(int i = sqrt(jeremy.graph->num_vertices); i < jeremy.graph->num_vertices; i++){
     mec.m = i;
     if (is_possible(jeremy.graph, mec.c, mec)){
@@ -177,6 +136,5 @@ struct move_t play(struct move_t previous_move)
     }
   }
   mec.m =  sqrt(jeremy.graph->num_vertices) + 1;
-  //    printf("move: %ld\n", mec.m);
   return mec;   
 }
