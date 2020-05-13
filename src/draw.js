2b20afbedfe0817f832c8108bb5af9bc2e7e3f19
BOUDROUA Imad <Imad.Boudroua@bordeaux-inp.fr>
2020-05-13 13:07:14 +0200

    visualisation sous html, js terminé

---
+++ src/draw.js
@@ -0,0 +1,189 @@
+var p = new Object();
+p.color = 0;
+p.move = [0,1,2,3,4,5,6,7,8,9,10,16,20,44,50,65,74,98,102,118,133,134,135,136,137,138,139,140,141,142,143];
+var pl = new Object();
+pl.color = 1;
+pl.move = [11,12,23,24,35,36,47,48,59,60,71,72,83,84,95,96,107,108,119,120,121,122,123,124,125,126,127,128,129,130,131,132];
+var size =  11;
+(function () {
+  var canvas = document.getElementById('hexmap');
+
+  var hexHeight,
+    hexRadius,
+    hexRectangleHeight,
+    hexRectangleWidth,
+    hexagonAngle = 0.523598776, // 30 degrees in radians
+    sideLength = 10,
+    boardWidth = 100,
+    boardHeight = 100;
+  player1 = 2;
+  size = size + 1;
+  var color1,
+    color2;
+  if (pl["color" == 1]) {
+    color1 = "green";
+    color2 = "red";
+  }
+  else {
+    color2 = "green";
+    color1 = "red";
+  }
+
+  var ctx = canvas.getContext('2d');
+  hexHeight = Math.sin(hexagonAngle) * sideLength;
+  hexRadius = Math.cos(hexagonAngle) * sideLength;
+  hexRectangleHeight = sideLength + 2 * hexHeight;
+  hexRectangleWidth = 2 * hexRadius;
+
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "#000000";
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 1;
+
+    drawBoard(ctx, boardWidth, boardHeight, size);
+    play(ctx, boardWidth, boardHeight, pl, p);
+
+    canvas.addEventListener("mousemove", function (eventInfo) {
+      var x,
+        y,
+        hexX,
+        hexY,
+        screenX,
+        screenY,
+        rect;
+
+      rect = canvas.getBoundingClientRect();
+
+      x = eventInfo.clientX - rect.left;
+      y = eventInfo.clientY - rect.top;
+
+      hexY = Math.floor(y / (hexHeight + sideLength));
+      hexX = Math.floor((x - (hexY % 2) * hexRadius) / hexRectangleWidth);
+
+      screenX = hexX * hexRectangleWidth + ((hexY % 2) * hexRadius);
+      screenY = hexY * (hexHeight + sideLength);
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      drawBoard(ctx, boardWidth, boardHeight);
+
+      // Check if the mouse's coords are on the board
+      if (hexX >= 0 && hexX < boardWidth) {
+        if (hexY >= 0 && hexY < boardHeight) {
+          ctx.fillStyle = "#000000";
+          drawHexagon(ctx, screenX, screenY, true);
+        }
+      }
+    });
+  }
+
+  function drawBoard(canvasContext, width, height, size) {
+    var i,
+      j;
+
+    for (i = 0; i < size; ++i) {
+      for (j = 0; j < size; ++j) {
+        drawHexagon(
+          ctx,
+          i * hexRectangleWidth + (j * hexRadius),
+          j * (sideLength + hexHeight),
+          false,
+          'white'
+        );
+      }
+    }
+    for (i = 0; i < size; ++i) {
+      for (j = 1; j < size; ++j) {
+        drawHexagon(
+          ctx,
+          0 * hexRectangleWidth + (j * hexRadius),
+          j * (sideLength + hexHeight),
+          false,
+          'red'
+        );
+        drawHexagon(
+          ctx,
+          i * hexRectangleWidth + (0 * hexRadius),
+          0 * (sideLength + hexHeight),
+          false,
+          'green'
+        );
+
+        drawHexagon(
+          ctx,
+          (size - 1) * hexRectangleWidth + (j * hexRadius),
+          j * (sideLength + hexHeight),
+          false,
+          'red'
+        );
+        drawHexagon(
+          ctx,
+          i * hexRectangleWidth + ((size - 1) * hexRadius),
+          (size - 1) * (sideLength + hexHeight),
+          false,
+          'green'
+        );
+      }
+    }
+    drawHexagon(
+      ctx,
+      (size - 1) * hexRectangleWidth + (0 * hexRadius),
+      0 * (sideLength + hexHeight),
+      false,
+      'red'
+    );
+  }
+
+  function drawHexagon(canvasContext, x, y, fill, color) {
+    var fill = fill || false;
+
+    canvasContext.beginPath();
+    canvasContext.strokeStyle = 'black';
+    canvasContext.moveTo(x + hexRadius, y);
+    canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight);
+    canvasContext.lineTo(x + hexRectangleWidth, y + hexHeight + sideLength);
+    canvasContext.lineTo(x + hexRadius, y + hexRectangleHeight);
+    canvasContext.lineTo(x, y + sideLength + hexHeight);
+    canvasContext.lineTo(x, y + hexHeight);
+    canvasContext.fillStyle = color;
+    canvasContext.fill();
+    canvasContext.closePath();
+
+    if (fill) {
+      canvasContext.fill();
+    } else {
+      canvasContext.stroke();
+    }
+  }
+  function play(canvasContext, width, height, player1, player2) {
+    var i = player2["move"][0][0];
+    var l = (player2["move"]).length
+    console.log(Math.trunc(player1["move"][5] / size));
+    var m;
+    for (m = 0; m < l; ++m) {
+      drawHexagon(
+        ctx,
+        Math.trunc(player2["move"][m] - size * Math.trunc(player2["move"][m] / size)) * hexRectangleWidth + (Math.trunc(player2["move"][m] / size) * hexRadius),
+        Math.trunc(player2["move"][m] / size) * (sideLength + hexHeight),
+        false,
+        color1
+      );
+
+
+    } for (m = 0; m < l; ++m) {
+      drawHexagon(
+        ctx,
+        Math.trunc(player1["move"][m] - size * Math.trunc(player1["move"][m] / size)) * hexRectangleWidth + (Math.trunc(player1["move"][m] / size) * hexRadius),
+        Math.trunc(player1["move"][m] / size) * (sideLength + hexHeight),
+        false,
+        color2
+      );
+
+
+    }
+
+  }
+
+})();
--- src/server.c
+++ src/server.c
@@ -7,15 +7,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Global seed for the random number generator
-static int Length = 8;
+static int Length = 5;
 static char Shape = 'h';
 static int show = 0;
 
 ////////////////////////////////////////////////////////////////
 // Function for parsing the options of the program
-// Currently available options are :
-// -s <seed> : sets the seed
+
 void parse_opts(int argc, char *argv[])
 {
   int opt;
@@ -46,7 +44,7 @@ void parse_opts(int argc, char *argv[])
 int is_move_possible(struct graph_t *g, int color, struct move_t move)
 {
   int n = (int)move.m;
-  if (n != -1 && (gsl_spmatrix_get(g->o, color, n) == 0) && (gsl_spmatrix_get(g->o, 1-color, n) == 0))
+  if (n != -1 && (gsl_spmatrix_get(g->o, color, n) == 0) && (gsl_spmatrix_get(g->o, 1 - color, n) == 0))
   {
     return 1;
   }
@@ -77,13 +75,27 @@ struct player_server *compute_next_player(struct player_server *p1, struct playe
   return p2;
 }
 
+void moves_player(struct graph_t *graph, int *t, int *size, struct player_server *p)
+{
+  int m = graph->num_vertices;
+  for (int i = 0; i < m; i++)
+  {
+    if ((gsl_spmatrix_get(graph->o, 1 - p->color, i) == 0 && gsl_spmatrix_get(graph->o, p->color, i) == 1) || (gsl_spmatrix_get(graph->o, 1 - p->color, i) == 0 && gsl_spmatrix_get(graph->o, p->color, i)))
+    {
+      t[size[0]] = i;
+      size[0]++;
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
-
-  //printf("*********paramètres du jeu**********");
+  printf("*********paramètres du jeu**********");
   parse_opts(argc, argv);
-  //printf("\nLength : %d\n", Length);
-  //printf("shape : %d\n", Shape);
+  printf("\nLength : %d\n", Length);
+  printf("shape : %c\n", Shape);
+  printf("***********************************\n");
+
   //-------------------------------------------
   struct graph_t *graph = new__graph_t(Length, Shape);
 
@@ -133,7 +145,6 @@ int main(int argc, char *argv[])
 
   while (1)
   {
-
     srand(time(NULL));
 
     if ((Shape == 'c' || Shape == 't') && count == size__graph_t(graph))
@@ -145,14 +156,10 @@ int main(int argc, char *argv[])
     p = compute_next_player(p1, p2, &move);
     move = p->play(move);
 
-    //printf("Turn : player %s plays the box %ld\n", p->name, move.m);
-
     if (is_move_possible(graph, p->color, move))
     {
-
       if (is_winning(graph, p->color, move, Shape))
       {
-
         coloriate__graph_t(graph, p->color, move);
         printf("The winner is %s\n", p->name);
         break;
@@ -172,17 +179,56 @@ int main(int argc, char *argv[])
 
     if (show)
     {
-
       print_graph(graph, Shape);
       //sleep(1);
       printf("\033[%dA", Length + 3);
       printf("\033[10B"); // Move down X lines;
     }
   }
+
   if (show)
     print_graph(graph, Shape);
   else if (equal)
     printf("Equality between players\n");
+
+  // draw the game on html page. Works just for m=3. Use the option -m=3 and open the html file to see the drawn game.
+  // It is also possible to draw the game with option -p1. Works fine for every m.
+  char ch;
+  FILE *fp;
+  FILE *fptr;
+
+  fp = fopen("src/test.js", "r");   // read mode
+  fptr = fopen("src/draw.js", "w"); // read mode
+
+  if (fp == NULL || fptr == NULL)
+  {
+    perror("Error while opening the file.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  fprintf(fptr, "var p = new Object();\np.color = %d;\np.move = [", p1->color);
+  int *tableau1 = malloc(sizeof(int) * Length * Length);
+  int size[1];
+  size[0] = 0;
+  moves_player(graph, tableau1, size, p1);
+
+  int *tableau2 = malloc(sizeof(int) * Length * Length);
+  int size2[1];
+  size2[0] = 0;
+  printf("\n");
+  moves_player(graph, tableau2, size2, p2);
+
+  for (int k = 0; k < size[0] - 1; k++)
+    fprintf(fptr, "%d,", tableau1[k]);
+  fprintf(fptr, "%d];\n", tableau1[size[0] - 1]);
+
+  fprintf(fptr, "var pl = new Object();\npl.color = %d;\npl.move = [", p2->color); //[4,1],[3,2],[3,3],[3,4],[3,5],[3,6]];\n");
+  for (int k = 0; k < size2[0] - 1; k++)
+    fprintf(fptr, "%d,", tableau2[k]);
+  fprintf(fptr, "%d];\n", tableau2[size2[0] - 1]);
+  fprintf(fptr, "var size =  %d;\n", Length); //[4,1],[3,2],[3,3],[3,4],[3,5],[3,6]];\n");
+  while ((ch = fgetc(fp)) != EOF)
+    fputc(ch, fptr);
 
   p1->finalize();
   p2->finalize();
@@ -191,6 +237,11 @@ int main(int argc, char *argv[])
   dlclose(player2);
   free(p1);
   free(p2);
+  free(tableau2);
+  free(tableau1);
+  fclose(fp);
+  dlclose(player1);
+  dlclose(player2);
 
   return 0;
 }
